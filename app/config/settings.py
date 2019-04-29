2a7a9ba91dfdad1c8eb0d9ae1f2deb9bad6d7d04
kimdohwan <dosio0102@gmail.com>
2019-04-29 17:19:26 +0900

    [ADD] DB for chat.tests

--- app/config/settings.py
+++ app/config/settings.py
@@ -83,13 +83,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'TEST': {
+            'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3')
+        }
     }
 }
 
