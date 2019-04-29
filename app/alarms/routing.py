194a8302765c85b15609fb43d005e5e77a527efc
kimdohwan <dosio0102@gmail.com>
2019-04-30 00:48:19 +0900

    [FIX] alarms/chat의 routing.py 를 config.routing 에서 모두 처리하도록 변경

---
+++ app/alarms/routing.py
@@ -0,0 +1,8 @@
+# from django.urls import path, re_path
+#
+# from . import consumers
+
+# websocket_urlpatterns = [
+#     # path('ws/test/<username>/', consumers.UserTestConsumer),
+#     re_path(r'ws/test/(?P<username>.*)/', consumers.UserTestConsumer),
+# ]
--- app/config/routing.py
+++ app/config/routing.py
@@ -1,12 +1,26 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path, re_path
 
+import alarms.routing
 import chat.routing
+from alarms.consumers import UserTestConsumer
+from chat import consumers
+from chat.consumers import ChatConsumer
 
 application = ProtocolTypeRouter({
+    # chat app
     'websocket': AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
+        URLRouter([
+            # chat.routing.websocket_urlpatterns,
+            # alarms.routing.websocket_urlpatterns,
+            path('ws/chat/<room_name>/', ChatConsumer),
+            re_path(r'ws/test/(?P<username>.*)/', UserTestConsumer),
+        ])
     )
+
+    # # alarms app
+    # 'websocket': URLRouter(
+    #     alarms.routing.websocket_urlpatterns
+    # )
 })
